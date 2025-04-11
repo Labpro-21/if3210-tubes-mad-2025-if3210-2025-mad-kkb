c2ac7f4252f3e59de882adae31c5858e84c18268
Jimly Nur Arif <132258178+jimlynurarif@users.noreply.github.com>
2025-04-11 10:52:43 +0700

    feat: No need to re-login because it uses a refresh token.

--- app/src/main/java/com/kkb/purrytify/MainActivity.kt
+++ app/src/main/java/com/kkb/purrytify/MainActivity.kt
@@ -1,37 +1,40 @@
 package com.kkb.purrytify
 
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.kkb.purrytify.TokenStorage.refreshAccessTokenIfNeeded
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val context = this
-        val accessToken = TokenStorage.getToken(context)
-        val initialRoute = if (accessToken != null && isTokenValid(accessToken)) "home" else "login"
+        lifecycleScope.launch {
+            val loggedIn = refreshAccessTokenIfNeeded(context)
+            val initialRoute = if (loggedIn) "home" else "login"
 
-        setContent {
-            val navController = rememberNavController()
-
-            NavHost(navController = navController, startDestination = initialRoute) {
-                composable("login") {
-                    LoginScreen(onLoginSuccess = {
-                        navController.navigate("home") {
-                            popUpTo("login") { inclusive = true }
-                        }
-                    })
-                }
-                composable("home") {
-                    HomeScreen(navController = navController, currentRoute = "home")
-                }
-
-                composable("profile") {
-                    ProfileScreen(navController = navController, currentRoute = "profile")
+            setContent {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = initialRoute) {
+                    composable("login") {
+                        LoginScreen(onLoginSuccess = {
+                            navController.navigate("home") {
+                                popUpTo("login") { inclusive = true }
+                            }
+                        })
+                    }
+                    composable("home") {
+                        HomeScreen(navController = navController, currentRoute = "home")
+                    }
+                    composable("profile") {
+                        ProfileScreen(navController = navController, currentRoute = "profile")
+                    }
                 }
             }
         }
